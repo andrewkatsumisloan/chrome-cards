d8e288d5ba84dd65140ea1773de33f17c633cfbd
Andrew Sloan <andrewkatsumisloan@gmail.com>
2022-11-13 13:12:56 -0800

    Stuff

---
+++ src/Data/StackData.js
@@ -0,0 +1,52 @@
+export const dummyStackData = [
+  {
+    id: 1,
+    front: "United States of America",
+    back: "Washington, D.C.",
+  },
+  {
+    id: 2,
+    front: "Canada",
+    back: "Ottawa",
+  },
+  {
+    id: 3,
+    front: "Japan",
+    back: "Tokyo",
+  },
+  {
+    id: 4,
+    front: "United Kingdom",
+    back: "London",
+  },
+  {
+    id: 5,
+    front: "France",
+    back: "Paris",
+  },
+  {
+    id: 6,
+    front: "Germany",
+    back: "Berlin",
+  },
+  {
+    id: 7,
+    front: "Italy",
+    back: "Rome",
+  },
+  {
+    id: 8,
+    front: "Spain",
+    back: "Madrid",
+  },
+  {
+    id: 9,
+    front: "Australia",
+    back: "Canberra",
+  },
+  {
+    id: 10,
+    front: "Brazil",
+    back: "Brasilia",
+  },
+];
